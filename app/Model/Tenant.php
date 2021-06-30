c9f998b2e61e95ea2f648bb66675c17b055a1c14
amitoz deol <amitozdeol@gmail.com>
2021-06-30 15:39:09 -0400

    Removed myTenant scope and made it into global scope

--- app/Model/Tenant.php
+++ app/Model/Tenant.php
@@ -25,21 +25,15 @@ class Tenant extends Model
      */
     protected static function booted()
     {
+        //Tenants whose lease is still active
         static::addGlobalScope('lease_end', function (Builder $builder) {
             $builder->whereDate('tenant.lease_end', '>=', Carbon::now());
         });
-    }
-
-    /**
-     * Scope a query to only include tenants for current logged in user.
-     *
-     * @param  \Illuminate\Database\Eloquent\Builder  $query
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    public function scopeMyTenant($query)
-    {
-        return $query->join('property_unit', 'tenant.property_unit_id', '=', 'property_unit.id')
+        //Only include tenants for current logged in user.
+        static::addGlobalScope('users', function (Builder $builder) {
+            $builder->join('property_unit', 'tenant.property_unit_id', '=', 'property_unit.id')
                     ->join('property', 'property.id', '=', 'property_unit.property_id')
                     ->where('property.user_id', Auth::guard('api')->id());
+        });
     }
 }
