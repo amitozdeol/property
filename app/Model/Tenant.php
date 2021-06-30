8598d3b4b71602b2a45ac1b2a20d183567040cb4
amitoz deol <amitozdeol@gmail.com>
2021-06-30 15:35:21 -0400

    Global scope to ignore whose lease has been ended

--- app/Model/Tenant.php
+++ app/Model/Tenant.php
@@ -4,7 +4,9 @@ namespace App\Model;
 
 use App\Model\Property;
 use App\Model\PropertyUnit;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Tenant extends Model
@@ -15,6 +17,18 @@ class Tenant extends Model
      * @var string
      */
     protected $table = 'tenant';
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::addGlobalScope('lease_end', function (Builder $builder) {
+            $builder->whereDate('tenant.lease_end', '>=', Carbon::now());
+        });
+    }
 
     /**
      * Scope a query to only include tenants for current logged in user.
